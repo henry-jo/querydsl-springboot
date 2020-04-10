b3a3d4a51a28a694b6f46a030f7ce9d9c213d873
henry-jo <jobc0335@gmail.com>
2020-04-10 13:54:48 +0900

    init project

---
+++ src/main/java/com/henry/querydslspring/QuerydslSpringApplication.java
@@ -0,0 +1,13 @@
+package com.henry.querydslspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henry/querydslspring/QuerydslSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry.querydslspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
